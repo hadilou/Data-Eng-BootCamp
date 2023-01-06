b9c63ce76f34949ea7a4e7b2bc446378cc1551f9
Kayode_Hadilou <kayodehadilou@hotmail.com>
2023-01-06 17:03:05 +0200

    ingest_data from parquet to postgre

--- week1/ingest_data.py
+++ week1/ingest_data.py
@@ -1,17 +1,68 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[45]:
-
+import subprocess
+import argparse
+from time import time
+from os import system
 
 import pandas as pd
 from sqlalchemy import create_engine
 
-df = pd.read_parquet("yellow_tripdata_2022-01.parquet"
+def run_cmd(cmd: str, timeout=None):
+    """Run a bash command.
+    Args:
+        cmd (str): Command to run in bash
+        timeout (int): Timeout in seconds. Defaults to None.
+    """
+    try:
+        if isinstance(cmd, list):
+            proc = subprocess.run(cmd, timeout=timeout)
+        else:
+            proc = subprocess.run(cmd, shell=True, timeout=timeout)
+        if proc.returncode != 0:
+            print(
+                f"Command <<{proc.args}>> failed.\nReturn Code: {proc.returncode}.\nError Message: {proc.stderr}\n")
+            exit()
+        else:
+            print(
+                f"Command <<{proc.args}>> failed.\nReturn Code: {proc.returncode}.\nError Message: {proc.stderr}\n")
+            exit()
+    except Exception as e:
+        print(f"Command {cmd} failed with error: ", e)
+        exit()
 
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-                     
-df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+def main(args):
+
+    # downloading the data 
+    output_name = args.url.split('/')[-1]
+    cmd = f"wget {args.url} -O {output_name}"
+    engine = create_engine(f'postgresql://{args.user}:{args.password}@{args.host}:{args.port}/{args.db}')
+    run_cmd(cmd)
+    # system(cmd)
+    if args.url.endswith('.parquet'):
+        df = pd.read_parquet(output_name)
+    else:
+        df = pd.read_csv(output_name)
+
+
+    df.to_sql(name='yellow_taxi_data', con=engine, if_exists='append')
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Ingest parquet file to Postgre Server")
+
+    parser.add_argument('--user', required=True, help='Username for Postgres')
+    parser.add_argument('--password', required=True, help='Password for Postgres')
+    parser.add_argument('--port', required=True, help='Port for Postgres', type=str)
+    parser.add_argument('--host', required=True, help='host for Postgres')
+    parser.add_argument('--db', required=True, help='database name for Postgres')
+    parser.add_argument('--table_name', required=True, help='Table name for Postgres')
+    parser.add_argument('--url', required=True, help='Url of the parquet file')
+
+    args = parser.parse_args()
+
+    main(args)
+
 
 
 
